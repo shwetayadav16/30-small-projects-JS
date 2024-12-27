0c822021bc3919279a681698ccbf4ecbdc6dc144
Shweta Yadav <shwetayadav3490@gmail.com>
2024-12-27 21:07:37 +0530

    in progress

---
+++ Tech with Tim/4/index.js
@@ -0,0 +1,16 @@
+let secondsElapsed=0;
+let interval=null;
+const time =document.getElementById("time")
+
+function timer(){
+    secondsElasped++;
+}
+function startClock(){
+  interval=setInterval(timer,1000)
+}
+function stopClock(){
+    
+}
+function resetClock(){
+    
+}
