247e1d4e6a68a24ef415a4573c50b4446e2d062a
Shweta Yadav <shwetayadav3490@gmail.com>
2025-05-22 12:31:51 +0530

    ..

--- Tech with Tim/2/index.js
+++ Tech with Tim/2/index.js
@@ -3,7 +3,7 @@ function reverseString(str){
     return str.split("").reverse().join("")
 }
 function check(){
-    const value=input.value
+    const value=input.value;
     const reverse=reverseString(value)
     if(value===reverse){
         alert("PALINDROME")
